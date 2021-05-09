ed5c7bcc1db16f3ad5b489857c637d7f173125b8
gq <guanqinghz@163.com>
2021-05-09 21:35:44 +0800

    banner 详解

--- src/main/java/com/mooc/sb/SbApplication.java
+++ src/main/java/com/mooc/sb/SbApplication.java
@@ -4,9 +4,11 @@ import com.mooc.sb.initializer.SecondInitializer;
 import com.mooc.sb.ioc.ann.MyBeanImport;
 import com.mooc.sb.listener.SecondListener;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @MapperScan("com.mooc.sb.mapper")
@@ -18,5 +20,11 @@ public class SbApplication {
         springApplication.addInitializers(new SecondInitializer());
         springApplication.addListeners(new SecondListener());
         springApplication.run(args);*/
+
+/*        SpringApplication springApplication = new SpringApplication(SbApplication.class);
+        springApplication.setBanner(new ResourceBanner(new ClassPathResource("banner_bak.txt")));
+        springApplication.run(args);*/
+
+//        System.out.println("\u001B[32m :: Spring Boot :: ");
     }
 }
